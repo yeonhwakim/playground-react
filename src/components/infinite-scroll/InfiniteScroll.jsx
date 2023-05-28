d72e6c8fe691a4b397dde168f044b4ad54e60a24
yeonhwa <yeonhwa6717@gmail.com>
2023-05-28 15:18:01 +0900

    markup: Markup basic layout.

--- src/components/infinite-scroll/InfiniteScroll.jsx
+++ src/components/infinite-scroll/InfiniteScroll.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import "./index.css";
+
 function InfiniteScroll(props) {
-  return <div></div>;
+  return (
+    <div className="scorllBox">
+      <div className="box"></div>
+      <div className="bottom"></div>
+    </div>
+  );
 }
 
 export default InfiniteScroll;
