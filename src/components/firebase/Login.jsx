097d8d57f9e7fc8c3d818b4ffc658c4459faff65
yeonhwa <yeonhwa6717@gmail.com>
2023-06-19 20:43:42 +0900

    feat: login and logout using google account.

--- src/components/firebase/Login.jsx
+++ src/components/firebase/Login.jsx
@@ -1,9 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login(props) {
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+
+const firebaseConfig = {
+...
+};
+
+const app = initializeApp(firebaseConfig);
+const authService = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+function Login() {
+  const [isLogin, setIsLogin] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [userProfile, setUserProfile] = useState("");
+
+  const handleClickLogin = async () => {
+    await signInWithPopup(authService, provider);
+
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        console.log(user);
+        const { displayName, photoURL } = user;
+        setIsLogin(true);
+        setUserName(displayName);
+        setUserProfile(photoURL);
+      } else {
+        setIsLogin(false);
+      }
+    });
+  };
+
+  const handleClickLogout = async () => {
+    await signOut(authService);
+    setIsLogin(false);
+  };
   return (
     <div>
-      <button>LOGIN</button>
+      {isLogin ? (
+        <>
+          <button onClick={handleClickLogout}>LOGOUT</button>
+          <div>
+            <img src={userProfile} alt={`${userName} 프로필`} />
+            <span>{userName}</span>
+          </div>
+        </>
+      ) : (
+        <button onClick={handleClickLogin}>LOGIN</button>
+      )}
     </div>
   );
 }
