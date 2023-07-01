98b74ecb93e56a652e849d34f86ad306526ee978
yeonhwa <yeonhwa6717@gmail.com>
2023-07-01 11:16:19 +0900

    feat: Add 'handleClickIsExist' and 'handleClickNotexist'

--- src/components/firebase/Database.jsx
+++ src/components/firebase/Database.jsx
@@ -53,6 +53,43 @@ function Database() {
   const handleClickDelete = () => {
     remove(ref(db, "/users"));
   };
+  const handleClickNotExist = () => {
+    const dbRef = ref(db);
+    get(child(dbRef, "/users/-NYi3hGUVCkUck15q65a"))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          update(ref(db, "/users/-NYi3hGUVCkUck15q65a"), {
+            email: "yeonhwa67172@gmail.com",
+            isAdmin: false,
+          });
+        } else {
+          set(ref(db, "/users/-NYi3hGUVCkUck15q65a"), {
+            email: "yeonhwa67173@gmail.com",
+            isAdmin: false,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  const handleClickIsExist = () => {
+    const dbRef = ref(db);
+    get(child(dbRef, "/users/-NYi3hGUVCkUck15q65t"))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          update(ref(db, "/users/-NYi3hGUVCkUck15q65t"), {
+            email: "yeonhwa67172@gmail.com",
+            isAdmin: true,
+          });
+        } else {
+          console.log("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <div>
       <button onClick={handleClickCreate}>CREATE</button>
@@ -60,6 +97,8 @@ function Database() {
       <button onClick={handleClickUpdate}>UPDATE</button>
       <button onClick={handleClickAddList}>ADDLIST</button>
       <button onClick={handleClickDelete}>DELETE</button>
+      <button onClick={handleClickNotExist}>IFNOTEXIST,INSERT</button>
+      <button onClick={handleClickIsExist}>IFEXIST,UPDATE</button>
     </div>
   );
 }
